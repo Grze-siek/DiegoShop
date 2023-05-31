78417e401e125dd3a2447e94e303f4a724fd3561
Grzegorz Ryczkowski <ryczkowski.grzegorz@gmail.com>
2023-05-31 15:13:06 +0200

    Fixing bugs

--- backend/settings.py
+++ backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -216,3 +216,6 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 if os.getcwd() == '/app':
     DEBUG = False
+
+django_heroku.settings(locals())
+
